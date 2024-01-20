7a771ab6bc1b47f9fdee4abc3e27cfeb0012f5ae
Thomas Karabelas <thomas.karabelas.20@cnu.edu>
2024-01-20 16:29:27 -0500

    html

--- Test/index.php
+++ Test/index.php
@@ -1,3 +1,11 @@
 <?php
 echo "Hello World";
 ?>
+<!DOCTYPE html>
+<html lang = "en">
+    <header>
+        <title>Train Track </title>
+        <meta charset = "utf-8">
+        <meta name = "viewport">
+        <link rel = "stylesheet" href ="">
+<
